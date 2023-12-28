ba9b24f7754bfc607fb041345f3688cc9208abd8
Honey Lou L. Danila <tradebryant@gmail.com>
2023-12-28 17:05:00 +0800

    documents

--- public/report_system/reports.php
+++ public/report_system/reports.php
@@ -271,23 +271,363 @@
 				);
 				echo json_encode($json_data);
 
+			
+				elseif($_POST["action"] == "pdf_deceased"):
+
+					$where = "";
+					if(!empty($_POST["from_date"])):
+						$where = $where . " and contract_date >= '" . $_POST["from_date"] . "'";
+					endif;
+
+					if(!empty($_POST["to_date"])):
+						$where = $where . " and contract_date <= '" . $_POST["to_date"] . "'";
+					endif;
+					$month = "";
+					$day = "";
+					$total = 0;
+					$casket = query("SELECT * FROM burial_service_contract where 1=1 ".$where." order by contract_date desc");
+
+					$mpdf = new \Mpdf\Mpdf([
+						'mode' => 'utf-8', 'format' => 'FOLIO-P',
+						'margin_top' => 15,
+						'margin_left' => 5,
+						'margin_right' => 5,
+						'margin_bottom' => 5,
+						'margin_footer' => 1,
+						'default_font' => 'helvetica'
+					]);
+
+					$html = "";
+					$html .='
+					<link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css">
+					<link rel="stylesheet" href="AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+					<link rel="stylesheet" href="AdminLTE/dist/css/skins/_all-skins.min.css">
+					<style>
+					.table, th, td, thead, tbody{
+						border: 2px solid black !important;
+						padding: 8px !important;
+					}
+					</style>
+					<div class="text-center" ><img src="resources/stateresa_header.jpg" class="img-responsive"></div>
+					<hr>
+					<div class="row">
+					<div class="col-xs-4 co-sm-4">
+						<h4 style="font-weight: 900;" style="float:left;">DECEASED REPORT</h4>
+					</div>
+					';
+
+					if(!empty($_POST["from_date"])):
+						$html.="<div class='col-xs-4 col-sm-4'><h4 style='font-weight:700;'>From: ".readable_date($_POST["from_date"])."</h4></div>";
+					endif;
+					if(!empty($_POST["to_date"])):
+						$html.="<div class='col-xs-4 col-sm-4'><h4 style='font-weight:700;'>To: ".readable_date($_POST["to_date"])."</h4></div>";
+					endif;
+
+					$html.='</div>';
+					$html .='
+					<div class="box-body">
+					<table style="width: 100%;">
+					<tr style="background-color:#FFCCD5;">
+						<th colspan="2">Date</th>
+						<th colspan="3">Deceased</th>
+						<th rowspan="2">BSC No.</th>
+						</tr>
+						<tr style="background-color:#FFCCD5;">
+						<th>Month</th>
+						<th>Day</th>
+						<th>Last Name</th>
+						<th>First Name</th>
+						<th>Middle Name</th>
+						</tr>
+						<tbody style="border-color: #000 !important;">
+					';
+
+					foreach($casket as $row): 
+						$html.='<tr>';
+					
+						 if($month != full_month($row["contract_date"])):
+							$month = full_month($row["contract_date"]);
+							$html.="<td>".$month."</td>";
+						  else:
+							$html.="<td></td>"; 
+						  endif;
+						  if($day != date_day($row["contract_date"])):
+							$day = date_day($row["contract_date"]);
+							$html.="<td>".$day."</td>";
+						  else:
+							$html.="<td></td>"; 
+						  endif;
+					
+						$html.='<td>'.strtoupper($row["deceased_lastname"]).'</td>';
+						$html.='<td>'.strtoupper($row["deceased_firstname"]).'</td>';
+						$html.='<td>'.strtoupper($row["deceased_middlename"]).'</td>';
+						$html.='<td>'.strtoupper($row["contract_id"]).'</td>';
+						$html.='</tr>';
+					 endforeach;
+
+					 $html.='
+					 </tbody>
+					 </table>
+					 </div>
+					 
+					 <div style="position: absolute; bottom: 0; left: 0; right: 0; text-align: center; border-top:2px solid black;padding-top:20px;">
+					 <div class="row">
+						 <div class="col-xs-12">
+							 <p><i>"Family Satisfaction is our Prime Objective"</i></p>
+						 </div>
+					 </div>
+				 </div>
+					 
+				 
+				 
+					 ';
+
+
+
+	
+					// $base_url = the_base_url();
+					// 		$options = urlencode(serialize($_POST));
+					// 		$webpath = $base_url . "/sta_teresa/reports_page?action=pdf_deceased&options=".$options;
+					// 		// dump($webpath);
+					// 		$filename = "DECEASED_REPORT";
+					// 		// dump($filename);
+					// 		$path = "reports/".$filename.".pdf";
+					// 		$exec = '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe" -O portrait  "'.$webpath.'" '.$path.'';
+					// 		// dump($exec);
+					// 		exec($exec);
+					// 		$load[] = array('path'=>$path, 'filename' => $filename, 'result' => 'success');
+					// 		$json = array('info' => $load);
+					// 		echo json_encode($json);
+	
+							$filename = "DECEASED_REPORT";
+							// dump($filename);
+							$path = "reports/".$filename.".pdf";
+							$mpdf->WriteHTML($html);
+				$mpdf->Output($path, \Mpdf\Output\Destination::FILE);
+							// dump($exec);
+							$load[] = array('path'=>$path, 'filename' => $filename, 'result' => 'success');
+							$json = array('info' => $load);
+							echo json_encode($json);
+
+
+
 			elseif($_POST["action"] == "pdf_sales"):
 
-				$base_url = the_base_url();
+				// $base_url = the_base_url();
 	
-				$options = urlencode(serialize($_POST));
-				$webpath = $base_url . "/sta_teresa/reports_page?action=pdf_sales&options=".$options;
+				// $options = urlencode(serialize($_POST));
+				// $webpath = $base_url . "/sta_teresa/reports_page?action=pdf_sales&options=".$options;
+				// // dump($webpath);
+				// // dump($webpath);
+				// $filename = "SALES_REPORT";
+				// // dump($filename);
+				// $path = "reports/".$filename.".pdf";
+				// $exec = '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe" -O portrait  "'.$webpath.'" '.$path.'';
+				// // dump($exec);
+				// exec($exec);
+				// $load[] = array('path'=>$path, 'filename' => $filename, 'result' => 'success');
+				// $json = array('info' => $load);
+				// echo json_encode($json);
+
+
+				$mpdf = new \Mpdf\Mpdf([
+					'mode' => 'utf-8', 'format' => 'FOLIO-P',
+					'margin_top' => 15,
+					'margin_left' => 5,
+					'margin_right' => 5,
+					'margin_bottom' => 5,
+					'margin_footer' => 1,
+					'default_font' => 'helvetica'
+				]);
+
+				$Guarantors=[];
+				$guarantors = query("select * from guarantors");
+				foreach($guarantors as $row):
+				$Guarantors[$row["tbl_id"]] = $row;
+				endforeach;
+
+				$where = "";
+				if(!empty($_POST["from_date"])):
+					$where = $where . " and transaction_date >= '" . $_POST["from_date"] . "'";
+				endif;
+				if(!empty($_POST["to_date"])):
+						$where = $where . " and transaction_date <= '" . $_POST["to_date"] . "'";
+				endif;
+
+
+				$transaction_guarantors = query("select agency from transaction where 1=1 " . $where . " and agency is not null and agency != '' group by agency");
+				$transaction = query("SELECT transaction_date, payment_type, agency, SUM(amount) as amount FROM TRANSACTION WHERE 1=1 ".$where."
+				GROUP BY transaction_date, payment_type, agency ORDER BY transaction_date DESC");
+				// dump($transaction);
+
+				$Transactions = [];
+				foreach($transaction as $row):
+				$Transactions[$row["transaction_date"]]["transaction_date"] = $row["transaction_date"];
+				if($row["payment_type"] == "CASH")
+					$Transactions[$row["transaction_date"]][$row["payment_type"]] = $row;
+				else
+				$Transactions[$row["transaction_date"]][$row["agency"]] = $row;
+				
+				endforeach;
+
+
+				$count = count($transaction_guarantors);
+				// dump($count);
+				$Total = [];
+				foreach($transaction_guarantors as $row):
+				$Total[$row["agency"]]["total"] = 0;
+				endforeach;
+				$Total["CASH"] = 0;
+				$month = "";
+				$day = "";
+
+
+				$html = "";
+				$html .= '
+				<link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css">
+				<link rel="stylesheet" href="AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+				<link rel="stylesheet" href="AdminLTE/dist/css/skins/_all-skins.min.css">
+				<style>
+					.table, th, td, thead, tbody{
+						border: 2px solid black !important;
+						padding: 8px !important;
+					}
+					</style>
+					<div class="text-center"><img src="resources/stateresa_header.jpg" class="img-responsive"></div>
+					<hr>
+
+					<div class="row">
+					<div class="col-xs-3 co-sm-3">
+						<h4 style="font-weight: 900;" style="float:left;">SALES REPORT</h4>
+					</div>
+				';
+
+				if(!empty($_POST["from_date"])):
+					$html .= "<div class='col-xs-3 col-sm-3'><h4 style='font-weight:700;'>From: ".readable_date($_POST["from_date"])."</h4></div>";
+				endif;
+				if(!empty($_POST["to_date"])):
+					$html .="<div class='col-xs-3 col-sm-3'><h4 style='font-weight:700;'>To: ".readable_date($_POST["to_date"])."</h4></div>";
+				endif;
+				$html .= '</div>';
+
+
+				$html .= '
+				<div class="box-body">
+					<table style="width: 100%;">
+					<tr style="background-color:#FFCCD5;">
+						<th colspan="2">Date</th>
+						<th rowspan="2">Cash</th>
+						<th colspan="'.$count.'">Guarantee Letters</th>
+						</tr>
+						<tr style="background-color:#FFCCD5;">
+							<th>Month</th>
+                			<th>Day</th>';
+				foreach($transaction_guarantors as $row):
+					$html.='<th>'.$Guarantors[$row["agency"]]["guarantor"].'</th>';
+				endforeach;
+						$html .='
+						
+						</tr>
+					</tr>
+					<tbody>
+				';
+
+			foreach($Transactions as $row):
+					$html.='<tr>';
+  
+					
+					 if($month != full_month($row["transaction_date"])):
+						$month = full_month($row["transaction_date"]);
+						$html.='<td>'.$month.'</td>';
+					  else:
+						$html.='<td></td>';
+					  endif;
+  
+  
+					  if($day != date_day($row["transaction_date"])):
+						$day = date_day($row["transaction_date"]);
+						$html.='<td>'.$day.'</td>';
+					  else:
+						$html.='<td></td>';
+					  endif;
+				
+  
+  
+					$html .= '<td style="text-align: right;">';
+				
+					if(isset($row["CASH"])): 
+						$Total["CASH"] = $Total["CASH"] + $row["CASH"]["amount"]; 
+							$html.=to_peso($row["CASH"]["amount"]); 
+						else: 
+							$html.=''; 
+					endif;
+					
+					$html.='</td>';
+					foreach($transaction_guarantors as $g):
+					if(isset($row[$g["agency"]])):
+						  $Total[$g["agency"]]["total"] = $Total[$g["agency"]]["total"] + $row[$g["agency"]]["amount"];
+						
+							$html.='<td style="text-align: right;">'.to_peso($row[$g["agency"]]["amount"]).'</td>';
+					  else: 
+						$html.='<td></td>';
+					 endif;
+					 endforeach;
+					  $html.='</tr>';
+				 endforeach;
+
+				 $html.='
+				 </tbody>
+				 <tr style="background-color:#FFCCD5;">
+				   <th colspan="2" class="text-right">TOTAL</th>
+				   <th style="text-align: right;">'.to_peso($Total["CASH"]).'</th>
+				 ';
+
+				foreach($Total as $key => $value):
+					if($key != "CASH"):
+					  $html.='<th style="text-align: right;">'.to_peso($value["total"]).'</th>';
+					endif;
+				endforeach;
+
+				$html.='
+				</tr>
+				</table>
+				</div>
+		
+	
+				<div style="position: absolute; bottom: 0; left: 0; right: 0; text-align: center; border-top:2px solid black;padding-top:20px;">
+				  <div class="row">
+					  <div class="col-xs-12">
+						  <p><i>"Family Satisfaction is our Prime Objective"</i></p>
+					  </div>
+				  </div>
+				</div>
+				';
+
+
+
+				// $base_url = the_base_url();
+	
+				// $options = urlencode(serialize($_POST));
+				// $webpath = $base_url . "/sta_teresa/reports_page?action=pdf_sales&options=".$options;
 				// dump($webpath);
 				// dump($webpath);
-				$filename = "SALES_REPORT";
+				$filename = "THE_SALES_REPORT";
 				// dump($filename);
 				$path = "reports/".$filename.".pdf";
-				$exec = '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe" -O portrait  "'.$webpath.'" '.$path.'';
+
+				
+				$mpdf->WriteHTML($html);
+				$mpdf->Output($path, \Mpdf\Output\Destination::FILE);
+				// $filename = "CASKET_REPORT";
+				// $path = "reports/".$filename.".pdf";
+			
+				// $exec = '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe" -O portrait  "'.$webpath.'" '.$path.'';
 				// dump($exec);
-				exec($exec);
+				// exec($exec);
 				$load[] = array('path'=>$path, 'filename' => $filename, 'result' => 'success');
 				$json = array('info' => $load);
 				echo json_encode($json);
+
 
 
 			elseif($_POST["action"] == "pdf_casket"):
@@ -440,20 +780,7 @@
 						$json = array('info' => $load);
 						echo json_encode($json);
 
-			elseif($_POST["action"] == "pdf_deceased"):
-				$base_url = the_base_url();
-						$options = urlencode(serialize($_POST));
-						$webpath = $base_url . "/sta_teresa/reports_page?action=pdf_deceased&options=".$options;
-						// dump($webpath);
-						$filename = "DECEASED_REPORT";
-						// dump($filename);
-						$path = "reports/".$filename.".pdf";
-						$exec = '"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe" -O portrait  "'.$webpath.'" '.$path.'';
-						// dump($exec);
-						exec($exec);
-						$load[] = array('path'=>$path, 'filename' => $filename, 'result' => 'success');
-						$json = array('info' => $load);
-						echo json_encode($json);
+		
 
 			elseif($_POST["action"] == "pdf_collectibles"):
 						
